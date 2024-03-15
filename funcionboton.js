cdcfc316cf4f228155eca912393570a57bfdcf63
BorjaManterola <bjmanterola@miuandes.cl>
2024-03-15 17:10:19 -0300

    Terminado

--- funcionboton.js
+++ funcionboton.js
@@ -1,5 +1,4 @@
-function cambiarFrase() 
-{
+function cambiarFrase() {
     var texto = document.getElementById("texto-ingresado").value;
     document.getElementById("texto-cambiar").textContent = texto;
 }
